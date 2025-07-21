77de48002d5a1ef720beee156355b0434093ba60
Dev Valand <devvaland17@gmail.com>
2025-07-21 16:41:13 +0530

    Update

--- 1_Basics/4 Number&Math.js
+++ 1_Basics/4 Number&Math.js
@@ -32,5 +32,5 @@ const hundreds = 1000000;
 const min = 1;
 const max = 6;
 
-console.log(Math.floor(Math.random() * (max - min + 1)) + 1);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
 
